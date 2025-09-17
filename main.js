9966f8da8b6c4890549acbc76e828ea316142e66
Amailya <amalyamxitaryan2020@gmail.com>
2025-09-17 23:04:33 +0400

    developer

---
+++ main.js
@@ -0,0 +1,54 @@
+let tasks=[]
+
+//Add Task()
+function addTask(task){
+    if(task.trim() === "") return
+    tasks.push({text: task, completed: false})
+    renderTasks()
+}
+
+//deleteTask()
+
+function deleteTask(index){
+    tasks.splice(index, 1)
+    renderTasks()
+}
+
+//toggleTask()
+function toggleTask(index){
+ tasks[index].completed = true
+
+    renderTasks()
+
+}
+
+//renderTasks()
+
+function renderTasks(){
+    const tasklist = document.getElementById('tasklist')
+    tasklist.innerHTML = ""
+
+    tasks.forEach((task, index) =>{
+     const li = document.createElement('li')
+     li.className = task.completed ? "completed"  : ""
+     li.innerHTML = `<span>${task.text} </span>
+     <button onClick="toggleTask(${index})">✅</button>
+     <button onClick="deleteTask(${index})">🚮</button>`
+
+   tasklist.appendChild(li)
+    })
+    taskCount()
+}
+
+function taskCount(){
+    let count= tasks.length;
+    const countEL = document.getElementById("taskCount")
+    countEL.textContent = `ընդհանուր ${count}`
+}
+
+    document.getElementById('taskbutton').addEventListener('click', ()=>{
+        const input = document.getElementById('taskinput')
+        addTask(input.value)
+        input.value =""
+    })
+
